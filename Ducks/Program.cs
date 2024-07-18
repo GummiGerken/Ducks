6f9ed461bf9ddd73e3a57acb983db27abec6c3c4
TheCookingUnderground <147450385+TheCookingUnderground@users.noreply.github.com>
2024-07-18 17:08:01 -0400

    Enable interactive automatic (Auto) or client-side rendering (CSR) section

--- Ducks/Program.cs
+++ Ducks/Program.cs
@@ -4,7 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddRazorComponents();
+builder.Services.AddRazorComponents()
+       .AddInteractiveWebAssemblyComponents();
 
 var app = builder.Build();
 
@@ -15,7 +16,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+// this line was missing from Microsoft's documentation
+app.UseBlazorFrameworkFiles();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
@@ -28,5 +30,7 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.MapRazorComponents<App>();
+app.MapRazorComponents<App>()
+   .AddInteractiveWebAssemblyRenderMode()
+   .AddAdditionalAssemblies(typeof(Ducks.Client._Imports).Assembly);
 app.Run();
